6e5a4bcd0a940a2f347878ff213194b4442dcd9b
Selmarinel <selmarinel@gmail.com>
2017-02-13 15:09:49 +0200

    master example

--- examples/1/index.php
+++ examples/1/index.php
@@ -1,15 +1,7 @@
 <?php
-
-require_once "../vendor/autoload.php";
-
-$dotenv = new Dotenv\Dotenv(__DIR__ . DIRECTORY_SEPARATOR);
-$dotenv->load();
-$path = null;
-$name = null;
-
+require_once "../../vendor/autoload.php";
 $service = new \Saver\Saver(new \Saver\Services\Upload\CurlUploadService( new \Saver\Services\Files\LocalFile()));
-$service->saveFromUrl($_POST['url'], $path , $name);
-
+$service->saveFromUrl($_POST['url']);
 ?>
 <form method="post">
     <input type="text" name="url" value="<?=$_POST['url']?>">
